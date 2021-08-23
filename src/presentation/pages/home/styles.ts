aaa0355d37222318aafd5e73607e8f0ab54d1933
viniciusrodrigues1a <viniciusrodrigues.aro@gmail.com>
2021-08-23 18:28:23 -0300

    removing unused components

--- src/presentation/pages/home/styles.ts
+++ src/presentation/pages/home/styles.ts
@@ -18,17 +18,6 @@ export const HeaderCornerImage = styled.Image`
   right: -42px;
 `;
 
-export const HeaderTitle = styled.Text`
-  color: #6410e6;
-  font-size: 26px;
-`;
-
-export const HeaderSubtitle = styled.Text`
-  color: #ffffff;
-  font-size: 14px;
-  margin-top: 6px;
-`;
-
 type HabitWrapperProps = {
   lastHabit: boolean;
 };
