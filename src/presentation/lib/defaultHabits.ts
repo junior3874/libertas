1bf5c411f525bc19ff340e536c71f40a269b48e8
Julin ddd 011 <wellingtonjr53@outlook.com.br>
2021-08-27 19:57:59 -0300

    create a const because these options were being used in different archives

---
+++ src/presentation/lib/defaultHabits.ts
@@ -0,0 +1,9 @@
+const cigar = require("../assets/cigar.png");
+const coffee = require("../assets/coffee.png");
+const pill = require("../assets/pill.png");
+
+export const defaultHabitOptions = [
+  { img: cigar, title: "Cigarro" },
+  { img: coffee, title: "Café" },
+  { img: pill, title: "Drogas" },
+];
