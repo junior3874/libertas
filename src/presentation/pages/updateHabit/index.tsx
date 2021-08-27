58a679c84575ffb86a3fa26d3d2ea73dc38e9e47
Julin ddd 011 <wellingtonjr53@outlook.com.br>
2021-08-27 20:28:08 -0300

    implemented new methods because dropdown and dateinput were refactored

--- src/presentation/pages/updateHabit/index.tsx
+++ src/presentation/pages/updateHabit/index.tsx
@@ -1,12 +1,13 @@
 import { RouteProp, useNavigation, useRoute } from "@react-navigation/native";
 import React, { useContext } from "react";
 import { useState } from "react";
-import { View } from "react-native";
+import { Keyboard, View } from "react-native";
 import BtnGeneric from "../../components/btnGeneric";
 import Form from "../../components/Form";
 import GoBackButton from "../../components/GoBackButton";
 import HeaderText from "../../components/HeaderText";
 import { HabitContext } from "../../context/habit";
+import { defaultHabitOptions } from "../../lib/defaultHabits";
 import { StackParamList } from "../../routes";
 
 import { Container, FormWrapper, EditButtonWrapper } from "./styles";
@@ -14,16 +15,31 @@ import { Container, FormWrapper, EditButtonWrapper } from "./styles";
 export default function UpdateHabit() {
   const navigation = useNavigation();
   const route = useRoute<RouteProp<StackParamList, "UpdateHabit">>();
+  //==================================================================
+  // Contexts
+  //==================================================================
+
   const { updateHabit, removeHabit } = useContext(HabitContext);
+
+  //==================================================================
+  // States
+  //==================================================================
+
   const [name, setName] = useState("");
-  const [lastDate, setLastDate] = useState<Date | null>();
+  const [lastDate, setLastDate] = useState<Date | undefined>(undefined);
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const [date, setDate] = useState(new Date());
+  const [calendarVisible, setCalendarVisible] = useState(false);
+
+  //==================================================================
+  // Handlers
+  //==================================================================
 
   async function onUpdateHabit() {
     await updateHabit({
       currentName: route.params.currentName,
       newName: name,
-      performedLastDate: new Date(),
+      performedLastDate: lastDate,
     });
     navigation.goBack();
   }
@@ -33,8 +49,23 @@ export default function UpdateHabit() {
     navigation.goBack();
   }
 
+  const _setLastDate = (date: Date) => {
+    setDate(date);
+    setLastDate(date);
+  };
+  const closeCalendar = () => setCalendarVisible(false);
+  const openCalendar = () => {
+    setCalendarVisible(true);
+  };
+
   return (
-    <Container onPress={() => setIsDropdownOpen(false)} activeOpacity={1}>
+    <Container
+      onPress={() => {
+        setIsDropdownOpen(false);
+        Keyboard.dismiss();
+      }}
+      activeOpacity={1}
+    >
       <GoBackButton />
       <HeaderText.Title message="Editar hábito" />
       <HeaderText.Subtitle message="Escolha qual opção você quer editar" />
@@ -43,18 +74,25 @@ export default function UpdateHabit() {
         <Form.DropdownWrapper>
           <Form.Dropdown
             title="Selecione um hábito"
-            options={[{ img: "", title: "Title" }]}
+            options={defaultHabitOptions}
             isDropdownOpen={isDropdownOpen}
             onDropdownOpen={() => setIsDropdownOpen(true)}
             onDropdownClose={() => setIsDropdownOpen(false)}
             onSelectedOptionChange={(selectedOption) => setName(selectedOption)}
+            option={name}
           />
         </Form.DropdownWrapper>
 
         <View style={{ height: 36 }} />
 
         <Form.Label message="Última vez que você se rendeu a este hábito" />
-        <Form.DateInput />
+        <Form.DateInput
+          onPress={openCalendar}
+          value={date}
+          setLastDate={_setLastDate}
+          visible={calendarVisible}
+          setIsVisible={closeCalendar}
+        />
       </FormWrapper>
       <EditButtonWrapper>
         <BtnGeneric
