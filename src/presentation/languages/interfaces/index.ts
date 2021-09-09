0846afa4d6649f07bc8414bdedf8dd40b2b49e81
viniciusrodrigues1a <viniciusrodrigues.aro@gmail.com>
2021-09-09 18:17:09 -0300

    removing unused exports

--- src/presentation/languages/interfaces/index.ts
+++ src/presentation/languages/interfaces/index.ts
@@ -1,5 +1,3 @@
-export * from "./IHabitAlreadyExistsErrorLanguage";
-export * from "./IMissingParamsErrorLanguage";
 export * from "./IToastLanguage";
 export * from "./ICreateHabitControllerLanguage";
 export * from "./IIndexHabitControllerLanguage";
