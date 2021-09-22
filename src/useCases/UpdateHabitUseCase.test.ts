536ae360a1f43ed593ecffb6ed37228adec31716
viniciusrodrigues1a <viniciusrodrigues.aro@gmail.com>
2021-09-22 05:07:29 -0300

    fix: instantiating Habit from UpdateHabitUseCase
    This fixes the bug where you could update a Habit to have a date in the
    future. This happened because the entity Habit that contained the
    business rule to prevent that was not being instantiated.

--- src/useCases/UpdateHabitUseCase.test.ts
+++ src/useCases/UpdateHabitUseCase.test.ts
@@ -1,4 +1,5 @@
 import { mock } from "jest-mock-extended";
+import * as HabitModule from "../entities/Habit";
 import { HabitAlreadyExistsError, NoHabitFoundError } from "./errors";
 import { IShowHabitRepository, IUpdateHabitRepository } from "./interfaces";
 import { UpdateHabitUseCase } from "./UpdateHabitUseCase";
@@ -15,6 +16,10 @@ function makeSut() {
 }
 
 describe("Update Habit use-case", () => {
+  afterEach(() => {
+    jest.resetAllMocks();
+  });
+
   it("should update a Habit successfully", async () => {
     const { sut, updateHabitRepositoryMock, showHabitRepositoryMock } =
       makeSut();
@@ -32,10 +37,7 @@ describe("Update Habit use-case", () => {
 
     await sut.update(updateDTO);
 
-    expect(updateHabitRepositoryMock.update).toHaveBeenNthCalledWith(
-      1,
-      updateDTO
-    );
+    expect(updateHabitRepositoryMock.update).toHaveBeenCalledTimes(1);
   });
 
   it("should throw HabitAlreadyExistsError if new name is already in use", async () => {
@@ -69,6 +71,34 @@ describe("Update Habit use-case", () => {
 
     await expect(sut.update(updateDTO)).rejects.toThrow(
       new NoHabitFoundError()
+    );
+  });
+
+  it("should instantiate Habit so that its business rules can be applied", async () => {
+    const { sut, updateHabitRepositoryMock, showHabitRepositoryMock } =
+      makeSut();
+    const createdHabit = { name: "My habit", performedLastDate: new Date() };
+    const updateDTO = {
+      currentName: "My habit",
+      newName: "My habit's new name",
+    };
+    showHabitRepositoryMock.show.mockImplementation((name: string) => {
+      return new Promise((resolve, _) => {
+        if (name === createdHabit.name) resolve(createdHabit);
+        resolve(undefined);
+      });
+    });
+    const habitSpy = jest.spyOn(HabitModule, "Habit");
+    habitSpy.mockImplementationOnce(
+      (_name: string, _date: Date) => ({} as HabitModule.Habit)
+    );
+
+    await sut.update(updateDTO);
+
+    expect(habitSpy).toHaveBeenNthCalledWith(
+      1,
+      updateDTO.newName,
+      createdHabit.performedLastDate
     );
   });
 });
